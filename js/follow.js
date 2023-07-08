b22833db74ae970933eec3c07a50321abba3e254
ors-reyes <67842856+ors-reyes@users.noreply.github.com>
2023-07-08 01:37:10 -0400

    v10

--- js/follow.js
+++
@@ -1,8 +0,0 @@
-let cursor = document.querySelector('.tooltips');
-console.log(cursor)
-document.addEventListener('mousemove', (e)=>{
-    let x = e.clientX
-    let y = e.clientY
-    cursor.style.left = (x + 10) + 'px'
-    cursor.style.top = (y - 50) + 'px'
-})
