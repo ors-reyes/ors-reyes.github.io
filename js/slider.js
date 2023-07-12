fd4302ea816a66949403bbeaeb57e9be4aabfd3d
ors-reyes <67842856+ors-reyes@users.noreply.github.com>
2023-07-12 05:17:34 -0400

    v20

--- js/slider.js
+++ js/slider.js
@@ -1,43 +1,37 @@
-const slides = document.querySelector(".slides");
-const allSlides = document.querySelectorAll(".slide");
-const slidesLength = allSlides.length;
-const slideWidth = allSlides[0].offsetWidth;
-let index = 0;
-
-const firstSlide = allSlides[0];
-const lastSlide = allSlides[allSlides.length - 1];
-
-const cloneFirstSlide = firstSlide.cloneNode(true);
-const cloneLastSlide = lastSlide.cloneNode(true);
-
-slides.appendChild(cloneFirstSlide);
-slides.insertBefore(cloneLastSlide, firstSlide);
-
-slides.addEventListener("transitionend", checkIndex);
-
-function switchSlide(arg) {
-  slides.classList.add("transition");
-    if (arg == "next") {
-      slides.style.left = `${slides.offsetLeft - slideWidth}px`;
-      index++;
-    } else {
-      slides.style.left = `${slides.offsetLeft + slideWidth}px`;
-      index--;
-    }
+const slides = document.querySelector('.slides')
+function next(){
+  let firstSlide = document.querySelectorAll('.slide')[0]
+  slides.style.marginLeft = '-100%'
+  slides.style.transition = 'all 0.5s'
+  setTimeout(function(){
+    slides.style.transition = 'none'
+    slides.insertAdjacentElement('beforeend', firstSlide)
+    slides.style.marginLeft = '0'
+  },500)
 }
+// const btnNext = document.querySelector('.btnRight')
+// btnNext.addEventListener('click', ()=>next())
 
-function checkIndex() {
-  slides.classList.remove("transition");
-  if (index == -1) {
-    slides.style.left = `-${slidesLength * slideWidth}px`;
-    index = slidesLength - 1;
-  }
-  if (index == slidesLength) {
-    slides.style.left = `-${1 * slideWidth}px`;
-    index = 0;
-  }
-}
 const play = ()=>{
-  setInterval(()=> switchSlide("next"),15000)
+    setInterval(()=> next() ,10000)
 }
 play()
+
+
+// Intersection Observer
+ const boxes = document.querySelectorAll('.boxes')
+ const observer = new IntersectionObserver((entries)=>{
+     entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.children[0].classList.remove('noPlay')
+            entry.target.children[0].classList.add('play')
+        }else{
+            entry.target.children[0].classList.add('noPlay')
+            entry.target.children[0].classList.remove('play')
+        }
+    });
+ }, {threshold: 0.8})
+ boxes.forEach((box)=>observer.observe(box))
+
+
+ 
