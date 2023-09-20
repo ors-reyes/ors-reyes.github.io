0975568f52669c33dcdc788d5c13875d3081d675
ors-reyes <67842856+ors-reyes@users.noreply.github.com>
2023-09-20 04:14:31 -0400

    novLaser

--- laserplot/js/app.js
+++ laserplot/js/app.js
@@ -12,6 +12,10 @@ const navObserver = new IntersectionObserver((entradas)=>{
     header.style.top = '0px'
     header.style.transition = 'all 1s'
     entradas.forEach(entrada =>{
+        console.log("as")
+        let contSection = entrada.target.children[0]
+        contSection.classList.remove("ver")
+        contSection.style.display = 'none'
         if(entrada.isIntersecting){
             const id = `#${entrada.target.id}`
             if(inicio === id || fin === id){
@@ -22,13 +26,21 @@ const navObserver = new IntersectionObserver((entradas)=>{
             }
             history.pushState({}, entrada.target.innetText, id)
             navIcons.forEach(navIcon =>{
+                console.log(contSection)
                 navIcon.children[0].classList.remove("activeNav")
                 if(navIcon.hash === id){
+                    setTimeout(() => {
+                        contSection.style.display = 'block'
+                    }, 500);
+                    setTimeout(() => {
+                        contSection.classList.add("ver")
+                      }, 800);
                     navIcon.children[0].classList.add("activeNav")
                     let altura = navIcon.getBoundingClientRect().top
                     indicador.style.transform = `translateY(${altura - 70}px)`
                 }
             })
+            // console.log(entrada.target.childNodes[1].childNodes[3].childNodes[1].children[1])
         }
     })
 },{
